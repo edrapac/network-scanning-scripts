21c1e799af9912700887fff2b6290bc67c9059ee
edrapac <33971688+edrapac@users.noreply.github.com>
2025-12-21 14:51:03 -0600

    Add -nocolor option to amass command

--- dotfile.sh
+++ dotfile.sh
@@ -174,7 +174,7 @@ apex_expand_scope (){
   domain=$1
   mkdir  subfinder dnsrecon gobuster fqdns amass
   dnsrecon -d "$domain" > dnsrecon/"$logtime"-"$domain"-dnsrecon.txt
-  amass enum -d "$domain" -active -o amass/"$logtime"-"$domain"-output -timeout 10
+  amass enum  -nocolor -d "$domain" -active -o amass/"$logtime"-"$domain"-output -timeout 10
   cat amass/"$logtime"-"$domain"-output | awk '{printf $1; printf "\n"}' | grep "$domain" | uniq > amass/"$logtime"-"$domain"-amass.txt
   echo "$domain" | subfinder -silent > subfinder/"$logtime"-"$domain"-subfinder.txt
   gobuster dns --domain "$domain" --wordlist /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -q > gobuster/"$logtime"-"$domain"-gobuster-output.txt
